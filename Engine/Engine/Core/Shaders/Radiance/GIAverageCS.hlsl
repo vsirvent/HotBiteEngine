9ea294b536f28a874b2964fcb9aaee26c94df292
Vicente Sirvent Orts <vicentesirvent@gmail.com>
2024-11-24 23:58:25 +0100

    Optimized pixel overflow

--- Engine/Engine/Core/Shaders/Radiance/GIAverageCS.hlsl
+++ Engine/Engine/Core/Shaders/Radiance/GIAverageCS.hlsl
@@ -139,10 +139,11 @@ void main(uint3 DTid : SV_DispatchThreadID)
     case 1: {
         //Pass 1 horizontal convolution
         float2 dir = float2(1.0f, 0.0f);
+
         for (x = -k; x <= k; ++x) {
             int2 p = (pixel + x * dir);
-            if (p.x < 0) { p.x += k; }
-            if (p.x > input_dimensions.x) { p.x -= k; }
+            p.x += step(Epsilon, -p.x) * k;
+            p.x += step(Epsilon, p.x - input_dimensions.x) * -k;
 
             float2 p1_info_pixel = round(p * infoRatio);
             ww = 1.0f;
@@ -161,8 +162,8 @@ void main(uint3 DTid : SV_DispatchThreadID)
         float2 dir = float2(0.0f, 1.0f);
         for (x = -k; x <= k; ++x) {
             int2 p = (pixel + x * dir);
-            if (p.y < 0) { p.y += k; }
-            if (p.y > input_dimensions.y) { p.y -= k; }
+            p.y += step(Epsilon, -p.y) * k;
+            p.x += step(Epsilon, p.y - input_dimensions.y) * -k;
 
             float2 p1_info_pixel = round(p * infoRatio);
             ww = 1.0f;
@@ -186,7 +187,7 @@ void main(uint3 DTid : SV_DispatchThreadID)
     case 3: {
         //Pass 2 convolution failed again, make a minimal 2D pass
         [branch]
-        if (prev_w < 2.2f && dist_to_cam < 1000.0f) {
+        if (prev_w < 2.0f && dist_to_cam < 1000.0f) {
             k = kernel_size + full_kernel;
             for (x = -k; x <= k; ++x) {
                 for (y = -k; y <= k; ++y) {
